5bbac5fca8a68c990c5d2f54f92dc3337911a633
Ajay K <76083469+ajaypep@users.noreply.github.com>
2023-07-11 13:51:36 +0530

    Add api-alert component.

--- components/global-nav.tsx
+++ components/global-nav.tsx
@@ -13,6 +13,11 @@ export function GlobalNav({
     const params = useParams();
     const routes = [
         {
+            href: `/${params.storeId}`,
+            label: "Dashboard",
+            active: pathName === `/${params.storeId}`,
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: "Settings",
             active: pathName === `/${params.storeId}/settings`,
--- components/models/alert-model.tsx
+++ components/models/alert-model.tsx
@@ -6,41 +6,41 @@ import { Model } from "@/components/ui/model";
 import { Button } from "@/components/ui/button";
 
 interface AlertModelProps {
-    isOpen: boolean;
-    onClose: () => void;
-    onConfirm: () => void;
-    loading: boolean;
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  loading: boolean;
 }
 
 export const AlertModel: React.FC<AlertModelProps> = ({
-    isOpen,
-    onClose,
-    onConfirm,
-    loading
+  isOpen,
+  onClose,
+  onConfirm,
+  loading
 }) => {
-    const [isMounted, setIsMounted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
-    if (!isMounted) {
-        return null;
-    }
-    return (
-        <Model
-            title="Are you sure?"
-            description="This action is irreversible"
-            isOpen={isOpen}
-            onClose={onClose}
-        >
-            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                <Button disabled={loading} variant={"outline"} onClick={onClose}>
-                    Cancel
-                </Button>
-                <Button disabled={loading} variant={"destructive"} onClick={onConfirm}>
-                    Confirm
-                </Button>
-            </div>
-        </Model>
-    )
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) {
+    return null;
+  }
+  return (
+    <Model
+      title="Are you sure?"
+      description="This action is irreversible"
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+        <Button disabled={loading} variant={"outline"} onClick={onClose}>
+          Cancel
+        </Button>
+        <Button disabled={loading} variant={"destructive"} onClick={onConfirm}>
+          Confirm
+        </Button>
+      </div>
+    </Model>
+  )
 }
