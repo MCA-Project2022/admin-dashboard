9303bde52b63ed7be7e1885cd469b52424589409
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2023-08-15 23:47:23 +0530

    1. Add Total Revenue Card to dashboard. 2. Add Sales Card to dashboard. 3. Add Products In Stock Card to dashboard. 4. Add get-total-revenue.ts.

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,22 +1,69 @@
+import { Heading } from "@/components/ui/heading";
 import prismadb from "@/lib/prismadb";
+import { Separator } from "@/components/ui/separator";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { CreditCardIcon, IndianRupeeIcon, PackageIcon } from "lucide-react";
+import { priceFormatter } from "@/lib/utils";
+import { getTotalRevenue } from "@/actions/get-total-revenue";
 
 
 interface DashboardPageProps {
     params: { storeId: string }
 };
 
-
 const DashboardPage: React.FC<DashboardPageProps> = async ({
     params
 }) => {
-    const store = await prismadb.store.findFirst({
-        where: {
-            id: params.storeId
-        }
-    });
+    const totalRevenue = await getTotalRevenue(params.storeId);
+    const salesCount = () => { };
+    const stockCount = () => { };
     return (
-        <div>
-            Current Store: {store?.name}
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <Heading title="Dashboard" description="Overview of your store" />
+                <Separator />
+                <div className="grid gap-4 grid-cols-3">
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                                Total Revenue
+                            </CardTitle>
+                            <IndianRupeeIcon className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-3xl font-bold">
+                                {priceFormatter.format(totalRevenue)}
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                                Sales
+                            </CardTitle>
+                            <CreditCardIcon className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-3xl font-bold">
+                                +25
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                                Products In Stock
+                            </CardTitle>
+                            <PackageIcon className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-3xl font-bold">
+                                12
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
         </div>
     )
 }
