feebac8862f53a13af406bdc8e85521981d2344e
Ajay K <76083469+ajaypep@users.noreply.github.com>
2023-08-15 14:08:03 +0530

    fix: categoryId GET response include billboard.

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -17,6 +17,9 @@ export async function GET(_req: Request,
       where: {
         id: params.categoryId,
       },
+      include: {
+        billboard: true,
+      }
     });
     return NextResponse.json(category);
   } catch (error) {
