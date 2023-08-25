2b04dd5fc601366b73b9330bc255695163082ced
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2023-08-26 00:37:50 +0530

    Add api/stores/[storeId] GET respo fun

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -2,6 +2,30 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
+export async function GET(_req: Request,
+  { params }: {
+    params: { storeId: string }
+  }
+) {
+  try {
+
+    if (!params.storeId) {
+      return new NextResponse("Store ID is required", { status: 400 });
+    }
+
+    const size = await prismadb.store.findUnique({
+      where: {
+        id: params.storeId,
+      },
+    });
+    return NextResponse.json(size);
+  } catch (error) {
+    console.error('[STORE_GET]', error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+
 export async function PATCH(req: Request,
   { params }: {
     params: { storeId: string }
